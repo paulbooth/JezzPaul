531a98094c23538ce21de9febe4825a001a40390
Paul Booth <thephantompaulbooth@gmail.com>
2012-10-04 04:14:56 -0400

    checking background

--- jezzball.js
+++ jezzball.js
@@ -185,7 +185,7 @@ function drawBackground()
 	drawingContext.drawImage(backgroundImage, 0, 0, gameWidth, gameHeight);
 	}
 	catch(err) {
-		alert(err);
+		alert([gameWidth, gameHeight]);
 	    backgroundImage = null;
 	}
     } else {
