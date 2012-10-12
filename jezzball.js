54574e676cc2512bd881981ce10d233c7fecdeda
Paul Booth <thephantompaulbooth@gmail.com>
2012-10-12 19:09:28 -0400

    test alert for mobile

--- jezzball.js
+++ jezzball.js
@@ -667,7 +667,7 @@ function initialize()
 
     canvasElement.width = gameWidth;
     canvasElement.height = gameHeight;
-    alert($.browser.mobile)
+    alert("mobile:" + $.browser.mobile)
     if (!$.browser.mobile) {
 	    canvasElement.onmousedown = mousedown;
 	    canvasElement.onmousemove = mousemove;
