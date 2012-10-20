ffc21c28942661bc4bf9427e36a7adb8d6602dbb
Paul Booth <thephantompaulbooth@gmail.com>
2012-10-20 02:56:30 -0400

    Added faraway server and views

---
+++ jezzpaul_level_server.js
@@ -0,0 +1,7 @@
+var express = require('express'),
+    app = express();
+
+app.set('views', __dirname + '/views');
+app.get("/:level", function(req, res) {
+	res.render("level.jade", {level: level});
+})
