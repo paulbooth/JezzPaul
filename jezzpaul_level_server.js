c9b9df09e1bc4c0e668a6163f5cb7cccdb1ea95f
Paul Booth <thephantompaulbooth@gmail.com>
2012-10-20 03:09:08 -0400

    level fix on server

--- jezzpaul_level_server.js
+++ jezzpaul_level_server.js
@@ -3,6 +3,7 @@ var express = require('express'),
 
 app.set('views', __dirname + '/views');
 app.get("/:level", function(req, res) {
+	var level = req.params.level;
 	res.render("level.jade", {level: level});
 });
 app.listen(3333);
