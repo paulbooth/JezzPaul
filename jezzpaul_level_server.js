11f3d121ed70c9f4f7a976d9e392e673f9300e82
Paul Booth <thephantompaulbooth@gmail.com>
2012-10-20 03:07:33 -0400

    Made it listen

--- jezzpaul_level_server.js
+++ jezzpaul_level_server.js
@@ -4,4 +4,5 @@ var express = require('express'),
 app.set('views', __dirname + '/views');
 app.get("/:level", function(req, res) {
 	res.render("level.jade", {level: level});
-})
+});
+app.listen(3333);
