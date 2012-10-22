1195bb7b0c4eddf1c616d1108070a3102c8f0aec
Paul Booth <thephantompaulbooth@gmail.com>
2012-10-22 12:33:28 -0400

    don't think I can abuse settimeout that way..

--- jezzball.js
+++ jezzball.js
@@ -880,7 +880,9 @@ function facebookLogin() {
             lineGrowSpeed += 1.5;
             $('#fbloginconnect').hide();
 		    $('#fbloginconnectsuccess').show();
-		    setTimeout($('#fbloginconnectsuccess').hide, 3000);
+		    setTimeout(function() {
+		    	$('#fbloginconnectsuccess').hide();
+		    }, 3000);
         } else {
             // cancelled
         }
