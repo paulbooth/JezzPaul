49daae7ce6a035cf3bb27fd61be9196c78767135
Paul Booth <thephantompaulbooth@gmail.com>
2012-11-07 11:09:46 -0500

    logging on server

--- jezzpaul_level_server.js
+++ jezzpaul_level_server.js
@@ -5,6 +5,8 @@ app.set('views', __dirname + '/views');
 app.get("/:level", function(req, res) {
 	var level = req.params.level;
   var score = req.query['score'];
+  console.log(score);
+  console.log(req.query);
 	res.render("level.jade", {level: level, score: score});
 });
 app.listen(3333);
